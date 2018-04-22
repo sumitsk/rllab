e531690d29f7110023e5ff2762f995cec01ccd94
sumitsk <sumit.sks4@gmail.com>
2018-04-22 14:49:03 -0400

    added gym obs

--- rllab/envs/mujoco/ant_env.py
+++ rllab/envs/mujoco/ant_env.py
@@ -15,11 +15,14 @@ class AntEnv(MujocoEnv, Serializable):
     ORI_IND = 3
 
     def __init__(self, *args, **kwargs):
+        self.velocity_dir = 'posx'
+        self.use_gym_obs = False
+        self.use_gym_reward = False
+
         super(AntEnv, self).__init__(*args, **kwargs)
         Serializable.__init__(self, *args, **kwargs)
-        self.velocity_dir = 'posx'
-    
-    def get_forward_reward(self):
+
+    def get_forward_reward_rllab(self):
         #print(self.velocity_dir)
         
         comvel = self.get_body_comvel("torso")
@@ -35,7 +38,30 @@ class AntEnv(MujocoEnv, Serializable):
             raise NotImplementedError
         return forward_reward
 
+    def get_forward_reward_gym(self, posbefore, posafter):
+        vel = (posafter - posbefore) / 0.05
+
+        if self.velocity_dir == 'posx':
+            forward_reward = vel[0]
+        elif self.velocity_dir == 'posy':
+            forward_reward = vel[1] 
+        elif self.velocity_dir == 'negx':
+            forward_reward = -vel[0] 
+        elif self.velocity_dir == 'negy':
+            forward_reward = -vel[1] 
+        else:
+            raise NotImplementedError
+        return forward_reward
+     
+
     def get_current_obs(self):
+        if self.use_gym_obs:
+            return np.concatenate([
+                self.model.data.qpos.flat[2:],
+                self.model.data.qvel.flat,
+                np.clip(self.model.data.cfrc_ext, -1, 1).flat,
+            ])
+            
         return np.concatenate([
             self.model.data.qpos.flat,
             self.model.data.qvel.flat,
@@ -43,28 +69,32 @@ class AntEnv(MujocoEnv, Serializable):
             self.get_body_xmat("torso").flat,
             self.get_body_com("torso"),
         ]).reshape(-1)
-        '''  
-        return np.concatenate([
-            self.model.data.qpos.flat[2:],
-            self.model.data.qvel.flat,
-            np.clip(self.model.data.cfrc_ext, -1, 1).flat,
-        ])
-        '''
-    
+        
     def step(self, action):
+        posbefore = self.get_body_com("torso")
         self.forward_dynamics(action)
+        posafter = self.get_body_com("torso")
+
         '''
         comvel = self.get_body_comvel("torso")
         forward_reward = comvel[0]
         '''
-        forward_reward = self.get_forward_reward()
+        if self.use_gym_reward:
+            forward_reward = self.get_forward_reward_gym(posbefore, posafter)
+            ctrl_cost = .5 * np.square(action).sum()
+            contact_cost = 0.5 * 1e-3 * np.sum(
+                np.square(np.clip(self.model.data.cfrc_ext, -1, 1)))
+            survive_reward = 1.0
 
-        lb, ub = self.action_bounds
-        scaling = (ub - lb) * 0.5
-        ctrl_cost = 0.5 * 1e-2 * np.sum(np.square(action / scaling))
-        contact_cost = 0.5 * 1e-3 * np.sum(
-            np.square(np.clip(self.model.data.cfrc_ext, -1, 1))),
-        survive_reward = 0.05
+        else:    
+            forward_reward = self.get_forward_reward_rllab()
+            lb, ub = self.action_bounds
+            scaling = (ub - lb) * 0.5
+            ctrl_cost = 0.5 * 1e-2 * np.sum(np.square(action / scaling))
+            contact_cost = 0.5 * 1e-3 * np.sum(
+                np.square(np.clip(self.model.data.cfrc_ext, -1, 1))),
+            survive_reward = 0.05
+        
         reward = forward_reward - ctrl_cost - contact_cost + survive_reward
         state = self._state
         notdone = np.isfinite(state).all() \
